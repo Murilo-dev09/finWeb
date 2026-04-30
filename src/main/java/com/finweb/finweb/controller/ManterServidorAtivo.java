f2dfc2105e834b4229e7cbe8a0f0afae77fb9b4d
Murilo <murilomorales09@gmail.com>
2026-04-30 17:45:47 -0300

    Adiciona request mapping /health

--- src/main/java/com/finweb/finweb/controller/ManterServidorAtivo.java
+++ src/main/java/com/finweb/finweb/controller/ManterServidorAtivo.java
@@ -1,9 +1,11 @@
 package com.finweb.finweb.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("manter")
+@RestController
+@RequestMapping("manter")
 public class ManterServidorAtivo {
 
     @GetMapping
